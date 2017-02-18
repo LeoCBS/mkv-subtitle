5c6c2629a327d5fad442300c0ef17cdcc936c4f9
Leonardo Cesar Borges <leocborgess@gmail.com>
2017-02-18 15:12:23 -0200

    fixing some pep8 errors

--- mkv/attacher.py
+++ mkv/attacher.py
@@ -43,7 +43,9 @@ def attach(args):
                     try:
                         mkv_path_absolute = os.path.join(dir_path, _file)
                         file_converted = _file.replace(".mkv", "converted.mkv")
-                        subtitle_path = mkv_path_absolute.replace(".mkv", ".srt")
+                        subtitle_path = mkv_path_absolute.replace(
+                                ".mkv", ".srt"
+                        )
                         if check_charset_utf(subtitle_path):
                             print("converting subtitle to iso-8859-1")
                             iconv = "iconv -t UTF-8 -f ISO-8859-1 {} > {}.tmp"
@@ -57,12 +59,16 @@ def attach(args):
                                     _file + " " + _file.replace(".mkv", ".srt"))
                         print("converting ", _file)
                         os.system(mkvmerge_cli)
-                        converted_absolute_path = os.path.join(dir_path, file_converted)
+                        converted_absolute_path = os.path.join(
+                                dir_path,
+                                file_converted
+                        )
                         cp_command = "cp " + converted_absolute_path + " " + args.hdddest
                         print("copying to hdd")
                         os.system(cp_command)
                     except Exception as e:
                         print(e)
+
 
 if __name__ == '__main__':
     attach(parse_args())
